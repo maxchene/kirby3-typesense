5c7674319e5a71c3ab363c38c88753d18d9a7232
Maxime <maxime@kaliel.fr>
2023-03-12 22:52:40 +0100

    provide each hook since can't acces access page from $event->age()

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 use Kirby\Cms\App;
-use Kirby\Cms\Event;
 use Kirby\Cms\Page;
+use Maxchene\Typesense\TypesenseConfig;
 use Maxchene\Typesense\TypesenseDocument;
 
 @include_once __DIR__ . '/vendor/autoload.php';
@@ -12,35 +12,63 @@ App::plugin('maxchene/typesense', [
         'host' => 'localhost:8108'
     ],
     'siteMethods' => [
-        'typesenseSearch' => function (string $query, array $fields = []): array {
-            return $this->seoTitle()->isNotEmpty() ? $this->seoTitle() : $this->title();
+        'typesenseSearch' => function (string $query): array {
+            return [];
         }
     ],
     'hooks' => [
-        'page.*:after' => function (Event $event, Page $newPage) {
-
-            if (in_array($event->action(), ['update', 'delete', 'changeStatus', 'changeTitle', 'changeTemplate'])) {
-                $config = option('maxchene.typesense.templates');
-                $templates = array_keys($config);
-                $template = $newPage->template()->name();
-
-                // check that page template is in config file
-                if (in_array($template, $templates)) {
-                    $document = new TypesenseDocument($newPage, $config[$template]);
-
-                    if (
-                        $event->action() === 'update' ||
-                        $event->action() === 'changeTitle' ||
-                        $event->action() === 'changeTemplate' ||
-                        $event->action() === 'changeStatus' && in_array($newPage->status(), ['listed', 'unlisted'])
-                    ) {
-                        $document->upsert();
-                    } else {
-                        $document->delete();
-                    }
-
-                }
+        'page.update:after' => function (Page $newPage) {
+            if (TypesenseConfig::isIndexable($newPage) && $newPage->isPublished()) {
+                $document = new TypesenseDocument($newPage);
+                $document
+                    ->setNormalizer(TypesenseConfig::getNormalizer($newPage))
+                    ->upsert();
             }
+        },
+        'page.changeTitle:after' => function (Page $newPage) {
+            if (TypesenseConfig::isIndexable($newPage)) {
+                $document = new TypesenseDocument($newPage);
+                $document
+                    ->setNormalizer(TypesenseConfig::getNormalizer($newPage))
+                    ->upsert();
+            }
+        },
+        'page.delete:after' => function (bool $status, Page $page) {
+            if ($status) {
+                $document = new TypesenseDocument($page);
+                $document->delete();
+            }
+        },
+        'page.changeTemplate:after' => function (Page $newPage) {
+
+            $document = new TypesenseDocument($newPage);
+            // if new template is not indexable, remove document
+            if (!TypesenseConfig::isIndexable($newPage)) {
+                $document->delete();
+            } else {
+                $document
+                    ->setNormalizer(TypesenseConfig::getNormalizer($newPage))
+                    ->upsert();
+            }
+
+        },
+        'page.changeStatus:after' => function (Page $newPage, Page $oldPage) {
+
+
+            // if new status is published and page is indexable, upsert
+            if ($newPage->isPublished() && TypesenseConfig::isIndexable($newPage)) {
+                $document = new TypesenseDocument($newPage);
+                $document
+                    ->setNormalizer(TypesenseConfig::getNormalizer($newPage))
+                    ->upsert();
+            }
+
+            // if new status is unpublished, delete document
+            if ($newPage->isDraft()) {
+                $document = new TypesenseDocument($newPage);
+                $document->delete();
+            }
+
         }
     ]
 ]);
